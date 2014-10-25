50516ceca6a9d92e21bb665b53e81f05c5edf03f
Cryptobscur <checkeurobscur@gmail.com>
2014-10-25 22:02:50 +0200

    Implémentation de chiffrement_message
    Implémentation complète de la fonction chiffrement_message. Deux
    fonctions secondaires ont été implémentées pour plus de clarté
    (cleValable et estDansAlphabet)

--- decalage.c
+++ decalage.c
@@ -1,7 +1,44 @@
 #include "decalage.h"
+#include <string.h>
+
+typedef int boolean; // Ce type ne doit prendre que les valeurs 0 ou 1
+
+boolean cleValable(CLE k)
+{
+	if(k < 0)
+		return 0;
+	else return 1;
+}
+
+boolean estDansAlphabet(char caractere)
+{
+	if(caractere >= 'a' && caractere <= 'z')
+		return 1;
+	else return 0;
+}
 
 CERTIFICATION chiffrement_message(char* message, CLE k)
 {
+	if(!cleValable(k))
+		return 1;
+		
+	char caractere_lu;
+	char* debut_msg = message;
+	
+	while((caractere_lu = (*message)) != '\0')
+	{
+		if(!estDansAlphabet(caractere_lu)) // les caracteres supportés pour l'instant sont ceux de 'a' à 'z' (extension envisagée)
+			return 2;
+		
+		int intermediaire = caractere_lu + (k % 26);
+		
+		(intermediaire <= 'z') ? ((*message) = intermediaire) : ((*message) = ((intermediaire % ('z' + 1)) + 'a'));
+		
+		message++; // la chaîne provient du terminal et ne subit aucun traitement avant l'appel de cette fonction,
+				   // on est donc restreint par le caractère nul de fin de chaîne, et ainsi l'incrémention n'est pas censée donner d'overflow
+	}
+	
+	printf("\nLe chiffré est : %s\n", debut_msg);
 	return 0;
 }
 
