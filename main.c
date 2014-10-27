7c751ab0bb261f88cf5a4e625afbecc50694b29d
Cryptobscur <checkeurobscur@gmail.com>
2014-10-27 22:09:50 +0100

    Ajout du support du c/d pour les fichiers

--- main.c
+++ main.c
@@ -7,36 +7,89 @@ int main(int argc, char* argv[])
 {
 	if(argc != 4) // nom du programme + le mode + la clé + la chaîne à chiffrer (ou déchiffrer)
 	{
-		fprintf(stderr, "\nErreur : vous devez renseigner un mode (chiffrement ou déchiffrement), une clé et une chaîne\n");
+		fprintf(stderr, "\nErreur : vous devez renseigner un mode (chiffrement ou déchiffrement), une clé et une chaîne.\n\n");
 		exit(1);
 	}
 	
-	CERTIFICATION retour;
+	CERTIFICATION retour = -1;
+	char* res_mode;
 	
-	if(strcmp(argv[1], "-c") == 0)
-	{
-		retour = chiffrement_message(argv[3], atoi(argv[2])); // atoi ne prendra en compte que les caractères numériques de la clé
-	}
-	else if(strcmp(argv[1], "-d") == 0)
-	{
-		retour = dechiffrement_message(argv[3], atoi(argv[2])); // atoi ne prendra en compte que les caractères numériques de la clé																	
-	}
-	else
-	{
-		fprintf(stderr, "\nErreur : mode inconnu\n");
-		exit(2);
-	}
+	int l_mode = strlen(argv[1]);
 	
-	switch(retour)
+	if(l_mode == 2)
 	{
-		case 0: printf("\nVotre message à été chiffré avec succès !\n");
-				break;
+		if(strcmp(argv[1], "-c") == 0)
+		{
+			retour = chiffrement_message(argv[3], atoi(argv[2])); // atoi ne prendra en compte que les caractères numériques de la clé
+			res_mode = "chiffré";
+		}
+		else if(strcmp(argv[1], "-d") == 0)
+		{
+			retour = dechiffrement_message(argv[3], atoi(argv[2])); // atoi ne prendra en compte que les caractères numériques de la clé
+			res_mode = "déchiffré";																	
+		}
+		else
+		{
+			fprintf(stderr, "\nErreur : mode inconnu.\n\n");
+			exit(2);
+		}
 		
-		case 1: fprintf(stderr, "\nErreur : votre clé doit spécifier une valeur positive\n");
-				break;
-				
-		case 2: fprintf(stderr, "\nErreur : le chiffrement a échoué car un ou plusieurs caractères de la chaîne sont invalides.\n");
-				break;
+		switch(retour)
+		{
+			case 0: printf("\nVotre message à été %s avec succès !\nLe résultat est : %s.\n\n", res_mode, argv[3]); // la chaîne est modifée directement dans les fonctions
+					break;
+			
+			case 1: fprintf(stderr, "\nErreur : votre clé doit spécifier une valeur positive.\n\n");
+					break;
+					
+			case 2: fprintf(stderr, "\nErreur : le chiffrement a échoué car un ou plusieurs caractères de la chaîne sont invalides.\n\n");
+					break;
+			
+			default: break;
+		}
+	}
+	else if(l_mode == 3) // modes par fichier
+	{
+		if(strcmp(argv[1], "-cf") == 0)
+		{
+			retour = chiffrement_fichier(argv[3], atoi(argv[2])); // atoi ne prendra en compte que les caractères numériques de la clé
+			res_mode = "chiffré";
+		}
+		else if(strcmp(argv[1], "-cd") == 0)
+		{
+			retour = dechiffrement_fichier(argv[3], atoi(argv[2])); // atoi ne prendra en compte que les caractères numériques de la clé
+			res_mode = "déchiffré";
+		}
+		else
+		{
+			fprintf(stderr, "\nErreur : mode inconnu.\n\n");
+			exit(2);
+		}
+		
+		switch(retour)
+		{
+			case 0: printf("\nVotre message à été %s avec succès !\nLe résultat est disponible dans le fichier associé.\n\n", res_mode); // la chaîne est modifée directement dans les fonctions
+					break;
+			
+			case 1: fprintf(stderr, "\nErreur : votre clé doit spécifier une valeur positive.\n\n");
+					break;
+					
+			case 2: fprintf(stderr, "\nErreur : le chiffrement a échoué car un ou plusieurs caractères de la chaîne sont invalides.\n\n");
+					break;
+			
+			case 3: fprintf(stderr, "\nErreur : impossible d'ouvrir le fichier %s.\n\n", argv[3]);
+					break;
+			
+			case 4: fprintf(stderr, "\nErreur : impossible de créer le fichier du résultat.\n\n");
+					break;
+					
+			default: break;
+		}
+	}
+	else 
+	{
+		fprintf(stderr, "\nErreur : mode inconnu.\n\n");
+		exit(2);
 	}
 	
 	exit(0);
