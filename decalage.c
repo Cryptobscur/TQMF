486bc73f239d0834b50a2c5129254d60ea31d7b5
Cryptobscur <checkeurobscur@gmail.com>
2014-10-28 14:34:10 +0100

    Implémentation de déchiffrement_fichier
    Ajout également de fonctions locales + généralisation de la fonction de
    création de nom de fichier

--- decalage.c
+++ decalage.c
@@ -67,20 +67,34 @@ CERTIFICATION dechiffrement_message(char* message, CLE k)
 	return 0;
 }
 
-char* creerNomFichierChiffre(char* nom_fichier)
-{
-	int intermediaire = 0;
+char* creerNom(char* nom_fichier, char* ajout, char* suppression)
+{	
+	/* Initialisation de quelques variables pour le traitement */
 	
-	char* pos_nom = nom_fichier;
-	while((*pos_nom) != '\0')
+	int l_ajout = strlen(ajout);
+	int l_suppression = strlen(suppression);
+	int l_nom_fichier = strlen(nom_fichier);
+	
+	char* pos_nom = nom_fichier;	
+	pos_nom += l_nom_fichier;
+	
+	int intermediaire = l_nom_fichier;
+	
+	/* On supprime l'expression '(clair)' ou '(chiffre)', selon le mode, si elle est présente dans le nom du fichier */
+	
+	char* pos_etat = strstr(nom_fichier, suppression);
+	
+	if(pos_etat != NULL)
 	{
-		pos_nom++;
-		intermediaire++;
+		strcpy(pos_etat, (pos_etat + l_suppression));
+		l_nom_fichier -= l_suppression;
+		pos_nom -= l_suppression;
+		intermediaire = l_nom_fichier;
 	}
 	
-	int l_nom_fichier = intermediaire; // '\0' n'est pas compté
+	/* On ajoute l'expression '(chiffre)' ou '(clair)', selon le mode, au nom du fichier */
 	
-	char* nom_chiffre = malloc(sizeof(char) * (8 + l_nom_fichier + 1)); // les 8 caractères correspondent à '_chiffre'
+	char* nom_chiffre = malloc(sizeof(char) * (l_ajout + l_nom_fichier + 1));
 	
 	while((*pos_nom) != '.')
 	{
@@ -90,16 +104,36 @@ char* creerNomFichierChiffre(char* nom_fichier)
 		if(intermediaire <= 0) // si le fichier ne possède pas d'extension dans son nom
 		{
 			strcpy(nom_chiffre, nom_fichier);
-			strcat(nom_chiffre, "_chiffre");
+			strcat(nom_chiffre, ajout);
 			return nom_chiffre;
 		}
 	}
 	
 	strncpy(nom_chiffre, nom_fichier, intermediaire); // pas de caractère nul à la fin mais il est ajouté juste après
-	strcat(nom_chiffre, "_chiffre");
+	strcat(nom_chiffre, ajout);
 	strcat(nom_chiffre, pos_nom);
 	
 	return nom_chiffre;
+}
+
+char* creerNomFichier(char* nom_fichier, char mode)
+{
+	char* ajout;	
+	char* suppression;
+	
+	if(mode == 'c') // chiffrement
+	{
+		ajout = "(chiffre)";		
+		suppression = "(clair)";
+	}
+	else if(mode == 'd') // déchiffrement
+	{
+		ajout = "(clair)";	
+		suppression = "(chiffre)";
+	}
+	else return NULL; // n'a pas lieu d'arriver normalement...
+	
+	return creerNom(nom_fichier, ajout, suppression);
 }
 
 CERTIFICATION chiffrement_fichier(char* nom_fichier, CLE k)
@@ -108,11 +142,14 @@ CERTIFICATION chiffrement_fichier(char* nom_fichier, CLE k)
 	if(f_message == NULL)
 		return 3;
 	
-	char* nom_fichier_chiffre = creerNomFichierChiffre(nom_fichier);
+	char* nom_fichier_chiffre = creerNomFichier(nom_fichier, 'c');
 	
 	FILE* f_chiffre = fopen(nom_fichier_chiffre, "w");
 	if(f_chiffre == NULL)
+	{
+		fclose(f_message);
 		return 4;
+	}
 	
 	char tampon[1024];
 	int nb_carac_lus;
@@ -143,5 +180,42 @@ CERTIFICATION chiffrement_fichier(char* nom_fichier, CLE k)
 
 CERTIFICATION dechiffrement_fichier(char* nom_fichier, CLE k)
 {
+	FILE* f_chiffre = fopen(nom_fichier, "r");
+	if(f_chiffre == NULL)
+		return 3;
+	
+	char* nom_fichier_clair = creerNomFichier(nom_fichier, 'd');
+	
+	FILE* f_message = fopen(nom_fichier_clair, "w");
+	if(f_message == NULL)
+	{
+		fclose(f_chiffre);
+		return 4;
+	}
+	
+	char tampon[1024];
+	int nb_carac_lus;
+	
+	CERTIFICATION retour;
+	
+	while((nb_carac_lus = fread(tampon, sizeof(char), 1024, f_chiffre)) > 0)
+	{
+		tampon[nb_carac_lus] = '\0'; // détermine la fin de la chaîne à chiffrer (caractères indéterminés par la suite)
+		
+		retour = dechiffrement_message(tampon, k);
+		
+		switch(retour)
+		{
+			case 0: fwrite(tampon, sizeof(char), nb_carac_lus, f_message);
+					break;
+			
+			default: fclose(f_chiffre);
+					 remove(nom_fichier_clair);
+					 return retour; // si on écrit pas de caractère, c'est qu'il y a eu un problème lors du chiffrement, et on renvoie donc l'erreur
+		}
+	}
+	
+	fclose(f_message);
+	fclose(f_chiffre);
 	return 0;
 }
